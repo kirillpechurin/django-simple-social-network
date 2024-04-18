0c0193ae4e35828105d59c6c0c820bd0dc3867cb
kirillpechurin <k.pechurin04@gmail.com>
2024-04-18 19:07:40 +0300

    Update handbooks

--- src/notifications/models/notification_event.py
+++ src/notifications/models/notification_event.py
@@ -4,6 +4,7 @@ from django.db import models
 class NotificationEvent(models.Model):
     class Handbook(models.IntegerChoices):
         BLOG_POSTS_LIKE = 1, "Like post"
+        BLOG_POSTS_NEW_COMMENT = 2, "Comment post"
 
     title = models.CharField(
         max_length=100
--- src/notifications/models/system_notification_type.py
+++ src/notifications/models/system_notification_type.py
@@ -4,6 +4,7 @@ from django.db import models
 class SystemNotificationType(models.Model):
     class Handbook(models.IntegerChoices):
         BLOG_POSTS_LIKE = 1, "Like posts"
+        BLOG_POSTS_COMMENTS = 2, "Comment posts"
 
     title = models.CharField(
         max_length=100
