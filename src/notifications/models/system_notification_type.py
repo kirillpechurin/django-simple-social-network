b0f6fb6c1020fb67039909238b8135df14f3e8db
kirillpechurin <k.pechurin04@gmail.com>
2024-04-18 21:29:46 +0300

    Update handbooks and fixtures

--- src/notifications/models/system_notification_type.py
+++ src/notifications/models/system_notification_type.py
@@ -5,6 +5,8 @@ class SystemNotificationType(models.Model):
     class Handbook(models.IntegerChoices):
         BLOG_POSTS_LIKE = 1, "Like posts"
         BLOG_POSTS_COMMENTS = 2, "Comment posts"
+        BLOG_SUBSCRIPTIONS = 3, "Blog Subscriptions"
+        BLOG_POSTS = 4, "Posts"
 
     title = models.CharField(
         max_length=100
